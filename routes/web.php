c4d0592e04040e386f64a460e622f6185592da64
Your Name <youremail@example.com>
2025-10-17 15:40:27 +0300

    routes of auth

---
+++ routes/web.php
@@ -0,0 +1,56 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+use App\Controllers\AuthController;
+use App\Controllers\UserController;
+use App\Core\BaseController;
+use App\Middleware\AuthMiddleware;
+
+// 🧭 Routes
+$router->add(
+    "GET",
+    "/users",
+    function () {
+        $user = AuthMiddleware::protect();
+        (new UserController())->getAll();
+    }
+); //Done
+$router->add(
+    "GET",
+    "/user",
+    function () {
+        $user = AuthMiddleware::protect();
+        $data = BaseController::getInput();
+        $id = $data["id"] ?? null;
+        $id = intval($id);
+        if ($id) (new UserController())->getUser($id);
+        else echo json_encode(["error" => "ID is required"]);
+    }
+); //Done
+// $router->add("POST", "/users", [new UserController(), "create"]);
+$router->add("DELETE", "/users/delete", function () {//done
+    $user = AuthMiddleware::protect();
+    $id = BaseController::getInput();
+    $id = intval($id["id"] ?? null);
+    // var_dump($id);
+    if ($id) (new UserController())->delete($id);
+    else echo json_encode(["error" => "ID is required"]);
+});
+$router->add("PUT", "/users/update", function () { //done
+    $id = BaseController::getInput();
+    $id = intval($id["id"] ?? null);
+
+    if ($id) (new UserController())->update($id);
+    else echo json_encode(["error" => "ID is required"]);
+});
+
+
+$router->add("POST", "/login", [new AuthController(), "login"]); //done
+$router->add("POST", "/register", [new AuthController(), "register"]); //done
+$router->add("POST", "/refresh", function () {
+    (new AuthController())->refreshToken();
+});//done
+$router->add("POST", "/logout", function () {
+    $user = AuthMiddleware::protect();
+    (new AuthController())->logout();
+});//done
