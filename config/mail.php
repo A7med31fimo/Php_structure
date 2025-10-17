4f57f06befd6b276b18eb6e4031bab802a805c2f
Your Name <youremail@example.com>
2025-10-17 15:58:35 +0300

    test email criteria

---
+++ config/mail.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'from_name' => 'Ahmed',
+    'from_address' => 'ahmed@gmail.com',
+    'protocol' => 'smtp', //smtp or sendmail
+    'encrypt' => 'ssl', //SSL or ssl | TLS or tls | NONE or none
+    'smtp_domain' => 'localhost',//or 0.0.0.0
+    'smtp_username' => '',
+    'smtp_password' => '',
+    'smtp_port' => '1025',
+];
