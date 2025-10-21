92c8df3a313d93e88e068dfd85ab58b0398ed188
Your Name <youremail@example.com>
2025-10-22 00:53:59 +0300

    edit index

--- index.php
+++ index.php
@@ -1,6 +1,27 @@
-
 <?php
-require "public/views/header.php";
-require "public/views/body.php";
-require "public/views/footer.php";
+// ini_set('display_errors', 1);
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers:Content-Type,Authorization");
+
+
+ob_start();
+session_start();
+
+require_once __DIR__ . '/vendor/autoload.php';
+use App\Core\Router;
+
+$router = new Router();
+require './routes/web.php';
+$router->run();
+
+
+
+if (!isset($_SESSION['user'])) {
+    header("Location: ./public/views/dashboard.php");
+    exit;
+}else{
+    header("Location: ./public/views/home.php");
+}
 ?>
