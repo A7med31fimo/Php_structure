639f1a9a3dbc0dc6ba07364a6882a7239ff20b0a
Your Name <youremail@example.com>
2025-10-25 01:42:25 +0300

    create routes for cv (CRUD operations)

--- routes/web.php
+++ routes/web.php
@@ -1,61 +1,84 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
 use App\Controllers\AuthController;
+use App\Controllers\CvController;
 use App\Controllers\UserController;
 use App\Helpers\Helper;
 use App\Middleware\AuthMiddleware;
 
-// Routes
+/**
+ * 
+ * @api Dashboard
+ * 
+ */
+$router->add("GET","/dashboard",function () {
+        header("Location: public/views/dashboard.php");
+    }
+);
 
-$router->add(
-    "GET",
-    "/users",
-    function () {
-        $user = AuthMiddleware::protect();
+/**
+ * 
+ * @api User auth
+ * 
+ */
+$router->add("GET","/users",function () {
+        AuthMiddleware::protect();
         (new UserController())->getAll();
     }
-); //Done
-$router->add(
-    "GET",
-    "/user",
-    function () {
-        $user = AuthMiddleware::protect();
+);
+
+$router->add("GET","/user",function () {
+        AuthMiddleware::protect();
         $data = Helper::getInput();
         $id = $data["id"] ?? null;
         $id = intval($id);
         if ($id) (new UserController())->getUser($id);
         else echo json_encode(["error" => "ID is required"]);
     }
-); //Done
-// $router->add("POST", "/users", [new UserController(), "create"]);
-$router->add("DELETE", "/users/delete", function () {//done
-    $user = AuthMiddleware::protect();
-    $id = Helper::getInput();
-    $id = intval($id["id"] ?? null);
-    // var_dump($id);
-    if ($id) (new UserController())->delete($id);
-    else echo json_encode(["error" => "ID is required"]);
+);
+
+$router->add("DELETE", "/users/delete", function () {
+        AuthMiddleware::protect();
+        $id = Helper::getInput();
+        $id = intval($id["id"] ?? null);
+        // var_dump($id);
+        if ($id) (new UserController())->delete($id);
+        else echo json_encode(["error" => "ID is required"]);
 });
-$router->add("PUT", "/users/update", function () { //done
-    $id = Helper::getInput();
-    $id = intval($id["id"] ?? null);
+$router->add("PUT", "/users/update", function () {
+        $id = Helper::getInput();
+        $id = intval($id["id"] ?? null);
 
-    if ($id) (new UserController())->update($id);
-    else echo json_encode(["error" => "ID is required"]);
+        if ($id) (new UserController())->update($id);
+        else echo json_encode(["error" => "ID is required"]);
 });
 
-
-$router->add("POST", "/login", [new AuthController(), "login"]); //done
-$router->add("POST", "/register", [new AuthController(), "register"]); //done
+/**
+ * 
+ * @api auth routes
+ * 
+ */
+$router->add("POST", "/login", [new AuthController(), "login"]);
+$router->add("POST", "/register", [new AuthController(), "register"]);
 $router->add("POST", "/refresh", function () {
     (new AuthController())->refreshToken();
-});//done
-$router->add("POST", "/logout", function () {
-    $user = AuthMiddleware::protect();
-    (new AuthController())->logout();
-});//done
-$router->add("GET", "/dashboard", 
-function (){
-     header("Location: public/views/dashboard.php");
 });
+$router->add("POST", "/verify", function () {
+    (new AuthController())->verify();
+});
+
+$router->add("POST", "/logout", function () {
+    AuthMiddleware::protect();
+    (new AuthController())->logout();
+});
+
+/**
+ * 
+ * @api Cv CRUD
+ * 
+*/
+$router->add("GET","/cv",[new CvController(),"getCv"]);
+$router->add("DELETE", "/cv/delete", [new CvController(),"delete"]);
+$router->add("PUT", "/cv/save", [new CvController(),"save"]);
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,6 +7,10 @@ namespace Composer\Autoload;
 class ComposerStaticInitbd59ad7baa3261f04af30759e1915cf6
 {
     public static $prefixLengthsPsr4 = array (
+        'P' => 
+        array (
+            'PHPMailer\\PHPMailer\\' => 20,
+        ),
         'F' => 
         array (
             'Firebase\\JWT\\' => 13,
@@ -18,6 +22,10 @@ class ComposerStaticInitbd59ad7baa3261f04af30759e1915cf6
     );
 
     public static $prefixDirsPsr4 = array (
+        'PHPMailer\\PHPMailer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
+        ),
         'Firebase\\JWT\\' => 
         array (
             0 => __DIR__ . '/..' . '/firebase/php-jwt/src',
